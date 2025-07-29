ee914fd81381a3353badcf8a7dd95d0e27fd127e
khushal tank <158576973+tankkhushal@users.noreply.github.com>
2025-07-29 15:46:41 +0530

    react js

---
+++ first-react-app/src/App.js
@@ -0,0 +1,17 @@
+import Hello from "./hello";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Hello/>
+        <p>
+          <h1>hello </h1>
+        </p>
+    
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <br>
+      <h1>raj</h1>
+      <h2>akoliya</h2>
+      <h3>Grid:6873</h3>
+      <h4>number:7861821691</h4>
+      <h4>city:surat</h4>
+      <h5>react js</h5>
+      <h6>hobby:gaming</h6>
+      <h7>gender:male</h7><br/><br/>
+      <img src="https://avatars.githubusercontent.com/u/178565240?s=400&v=4"></img>
+    </br>
+  )
+}
+
+export default App
